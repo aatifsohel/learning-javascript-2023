d58bbcb5e8f26f604cf5a056d00a9d1eb93cb9a7
Aatif Sohel <84847205+aatifsohel@users.noreply.github.com>
2022-12-13 15:35:50 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let js = "amazing";
+console.log(40 + 22 - 10);
+
+console.log('Aatif');
+console.log('23');
+
+// VARIABLES
+
+let firstName = "Aatif";
+console.log(firstName);
+
+// CONSTANTS
+
+let PI = 3.14;
+
+// DESCRIPTIVE VARIABLE NAMES
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
