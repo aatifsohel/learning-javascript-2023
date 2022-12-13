613326a0c78e0dd648120e8731f7dec49a3a99e7
Aatif Sohel <84847205+aatifsohel@users.noreply.github.com>
2022-12-13 15:59:55 +0530

    let, const and var

--- script.js
+++ script.js
@@ -1,30 +1,43 @@
-let js = "amazing";
-console.log(40 + 22 - 10);
+// let js = "amazing";
+// console.log(40 + 22 - 10);
 
-console.log('Aatif');
-console.log('23');
+// console.log('Aatif');
+// console.log('23');
 
-// VARIABLES
+// // VARIABLES
 
-let firstName = "Aatif";
-console.log(firstName);
+// let firstName = "Aatif";
+// console.log(firstName);
 
-// CONSTANTS
+// // CONSTANTS
 
-let PI = 3.14;
+// let PI = 3.14;
 
-// DESCRIPTIVE VARIABLE NAMES
+// // DESCRIPTIVE VARIABLE NAMES
 
-let myFirstJob = "Programmer";
-let myCurrentJob = "Teacher";
+// let myFirstJob = "Programmer";
+// let myCurrentJob = "Teacher";
 
-// Dynamic Typing
+// // Dynamic Typing
 
-let year;
-console.log(year);
-console.log(typeof year);
+// let year;
+// console.log(year);
+// console.log(typeof year);
 
-year = 1999;
-console.log(typeof year);
+// year = 1999;
+// console.log(typeof year);
 
-console.log(typeof null);
+// console.log(typeof null);
+
+// let, var, const
+
+let age = 30;
+age = 31;
+
+const birthYear = 1999;
+// birthYear = 1990;
+
+// const job;
+
+var job = "programmer";
+job = 'teacher';
