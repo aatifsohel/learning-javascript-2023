bc6e827bd77e1e6b5c083f82a7a1ca61eccfadd3
Aatif Sohel <84847205+aatifsohel@users.noreply.github.com>
2022-12-21 20:50:21 +0530

    switch statement

--- script.js
+++ script.js
@@ -194,21 +194,36 @@
 // Challenge 03 - Bonus 2
 
 
-const teamDolphinsScore = 97 + 112 + 101;
-const teamKoalasScore = 109 + 95 + 106;
+// const teamDolphinsScore = 97 + 112 + 101;
+// const teamKoalasScore = 109 + 95 + 106;
 
-const teamDolphinsAvgScore = teamDolphinsScore / 3;
-const teamKoalasAvgScore = teamKoalasScore / 3;
+// const teamDolphinsAvgScore = teamDolphinsScore / 3;
+// const teamKoalasAvgScore = teamKoalasScore / 3;
 
-console.log(teamDolphinsAvgScore, teamKoalasAvgScore);
-console.log(teamDolphinsScore, teamKoalasScore);
+// console.log(teamDolphinsAvgScore, teamKoalasAvgScore);
+// console.log(teamDolphinsScore, teamKoalasScore);
 
-if ((teamDolphinsAvgScore > teamKoalasAvgScore) && (teamDolphinsAvgScore >= 100)) {
-    console.log(`Team Dolphins is the winner with score ${teamDolphinsAvgScore}`);
-} else if ((teamKoalasAvgScore > teamDolphinsAvgScore) && (teamKoalasAvgScore >= 100)) {
-    console.log(`Team Koalas is the winner with score ${teamKoalasAvgScore}`);
-} else {
-    console.log('DRAW!!!');
+// if ((teamDolphinsAvgScore > teamKoalasAvgScore) && (teamDolphinsAvgScore >= 100)) {
+//     console.log(`Team Dolphins is the winner with score ${teamDolphinsAvgScore}`);
+// } else if ((teamKoalasAvgScore > teamDolphinsAvgScore) && (teamKoalasAvgScore >= 100)) {
+//     console.log(`Team Koalas is the winner with score ${teamKoalasAvgScore}`);
+// } else {
+//     console.log('DRAW!!!');
+// }
+
+
+const day = 'monday';
+
+switch (day) {
+    case 'monday':  // day === 'monday'
+        console.log('Plan course structure');
+        console.log('Go to coding meetup');
+        break;
+    case 'tuesday':
+        console.log('Prepare theory videos');
+        break;
+    default:
+        console.log('Not a valid day!');
 }
 
 
