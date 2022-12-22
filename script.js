ef1f15ae8d80c4f37ae52e4f5e38213a3f22b08a
Aatif Sohel <84847205+aatifsohel@users.noreply.github.com>
2022-12-22 19:37:14 +0530

    conditional (ternary) operator

--- script.js
+++ script.js
@@ -212,18 +212,36 @@
 // }
 
 
-const day = 'monday';
+// const day = 'monday';
 
-switch (day) {
-    case 'monday':  // day === 'monday'
-        console.log('Plan course structure');
-        console.log('Go to coding meetup');
-        break;
-    case 'tuesday':
-        console.log('Prepare theory videos');
-        break;
-    default:
-        console.log('Not a valid day!');
+// switch (day) {
+//     case 'monday':  // day === 'monday'
+//         console.log('Plan course structure');
+//         console.log('Go to coding meetup');
+//         break;
+//     case 'tuesday':
+//         console.log('Prepare theory videos');
+//         break;
+//     default:
+//         console.log('Not a valid day!');
+// }
+
+
+// Conditional Operator (ternary)
+
+const age = 23;
+age >= 18 ? console.log('I love writing code 😍') : console.log('I also like playing computer games ♥');
+
+const drink = age >= 18 ? 'wine' : 'water';
+console.log(drink);
+
+let drink2;
+if (age >= 18) {
+    drink2 = 'wine'
+} else {
+    drink2 = 'water'
 }
+console.log(drink2)
 
+console.log(`I like to drink ${age >= 18 ? 'wine' : 'water'}`);
 
