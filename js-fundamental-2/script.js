b3a51d9b03de3a4e290590bd93817c5b168cbfa9
Aatif Sohel <84847205+aatifsohel@users.noreply.github.com>
2022-12-23 15:51:39 +0530

    functions

--- js-fundamental-2/script.js
+++ js-fundamental-2/script.js
@@ -1,9 +1,35 @@
 'use strict';
 
-let hasDriversLicense = false;
-const passTest = true;
+// let hasDriversLicense = false;
+// const passTest = true;
 
-// if (passTest) hasDriversLicense = true;
-if (passTest) hasDriverLicense = true;
+// // if (passTest) hasDriversLicense = true;
+// if (passTest) hasDriverLicense = true;
 
-if (hasDriversLicense) console.log('I can drive :D')
+// if (hasDriversLicense) console.log('I can drive :D')
+
+
+// Functions
+
+function logger() {
+    console.log('This string is in logger function');
+}
+
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+// function with parameters
+
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+console.log(fruitProcessor(5, 0));
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
