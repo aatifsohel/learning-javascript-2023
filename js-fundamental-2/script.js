409d1816adcdd2fc4944cadc3234c3ea15a681a2
Aatif Sohel <84847205+aatifsohel@users.noreply.github.com>
2022-12-23 16:10:38 +0530

    function declarations vs expressions

--- js-fundamental-2/script.js
+++ js-fundamental-2/script.js
@@ -11,25 +11,45 @@
 
 // Functions
 
-function logger() {
-    console.log('This string is in logger function');
-}
+// function logger() {
+//     console.log('This string is in logger function');
+// }
 
 // calling / running / invoking function
-logger();
-logger();
-logger();
+// logger();
+// logger();
+// logger();
 
 // function with parameters
 
-function fruitProcessor(apples, oranges) {
-    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
-    return juice;
+// function fruitProcessor(apples, oranges) {
+//     const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//     return juice;
+// }
+
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
+// console.log(fruitProcessor(5, 0));
+
+// const appleOrangeJuice = fruitProcessor(2, 4);
+// console.log(appleOrangeJuice);
+
+
+// function declarations
+
+function calcAge(birthYear) {
+    return 2037 - birthYear;
 }
 
-const appleJuice = fruitProcessor(5, 0);
-console.log(appleJuice);
-console.log(fruitProcessor(5, 0));
+const age1 = calcAge(1999);
+console.log(age1);
 
-const appleOrangeJuice = fruitProcessor(2, 4);
-console.log(appleOrangeJuice);
+// function expression 
+
+const calcAge2 = function (birthYear) {
+    return 2037 - birthYear;
+}
+
+const age2 = calcAge2(1999);
+
+console.log(age1, age2)
