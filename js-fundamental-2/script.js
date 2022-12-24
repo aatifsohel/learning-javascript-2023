d45ea6f65c1c30f6fe63272d7f3784ba58ebaa18
Aatif Sohel <84847205+aatifsohel@users.noreply.github.com>
2022-12-24 18:33:26 +0530

    function calling other functions

--- js-fundamental-2/script.js
+++ js-fundamental-2/script.js
@@ -46,25 +46,40 @@
 
 // function expression
 
-const calcAge2 = function (birthYear) {
-    return 2037 - birthYear;
-}
+// const calcAge2 = function (birthYear) {
+//     return 2037 - birthYear;
+// }
 
 // const age2 = calcAge2(1999);
 // console.log(age1, age2)
 
-// Arrow Function 
+// Arrow Function
 
-const calcAge3 = birthYear => 2037 - birthYear;
-const age3 = calcAge3(1999);
-console.log(age3);
+// const calcAge3 = birthYear => 2037 - birthYear;
+// const age3 = calcAge3(1999);
+// console.log(age3);
 
-const yearsUntilRetirement = (birthYear, firstName) => {
-    const age = 2037 - birthYear;
-    const retirement = 65 - age;
-    // return retirement;
-    return `${firstName} retires in ${retirement} years`;
+// const yearsUntilRetirement = (birthYear, firstName) => {
+//     const age = 2037 - birthYear;
+//     const retirement = 65 - age;
+//     // return retirement;
+//     return `${firstName} retires in ${retirement} years`;
+// }
+
+// console.log(yearsUntilRetirement(1999, 'Aatif'));
+// console.log(yearsUntilRetirement(1990, 'John'));
+
+// Function calling other functions
+
+function cutFruitPieces(fruit) {
+    return fruit * 4;
 }
 
-console.log(yearsUntilRetirement(1999, 'Aatif'));
-console.log(yearsUntilRetirement(1990, 'John'));
+function fruitProcessor(apples, oranges) {
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+    const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`;
+    return juice;
+}
+
+console.log(fruitProcessor(2, 3));
