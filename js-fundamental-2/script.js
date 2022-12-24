4c5b30d9baf5070cbc3298473a844873a56bc918
Aatif Sohel <84847205+aatifsohel@users.noreply.github.com>
2022-12-24 18:22:30 +0530

    arrow functions

--- js-fundamental-2/script.js
+++ js-fundamental-2/script.js
@@ -37,19 +37,34 @@
 
 // function declarations
 
-function calcAge(birthYear) {
-    return 2037 - birthYear;
-}
+// function calcAge(birthYear) {
+//     return 2037 - birthYear;
+// }
 
-const age1 = calcAge(1999);
-console.log(age1);
+// const age1 = calcAge(1999);
+// console.log(age1);
 
-// function expression 
+// function expression
 
 const calcAge2 = function (birthYear) {
     return 2037 - birthYear;
 }
 
-const age2 = calcAge2(1999);
+// const age2 = calcAge2(1999);
+// console.log(age1, age2)
 
-console.log(age1, age2)
+// Arrow Function 
+
+const calcAge3 = birthYear => 2037 - birthYear;
+const age3 = calcAge3(1999);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    // return retirement;
+    return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearsUntilRetirement(1999, 'Aatif'));
+console.log(yearsUntilRetirement(1990, 'John'));
