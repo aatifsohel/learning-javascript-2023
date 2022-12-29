a3bd54a1c6a97cc622a09266c4902f5e63ac930a
Aatif Sohel <84847205+aatifsohel@users.noreply.github.com>
2022-12-29 22:18:20 +0530

    manipulate css styling

--- js-dom-events/script.js
+++ js-dom-events/script.js
@@ -14,10 +14,18 @@ document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
     console.log(typeof guess);
 
+    // when there is no input
     if (!guess) {
         document.querySelector('.message').textContent = '⛔ No number!'
+
+        // when player wins
     } else if (guess === secretNumber) {
         document.querySelector('.message').textContent = '🎉 Correct Number!';
+        document.querySelector('body').style.backgroundColor = '#60b347';
+
+        document.querySelector('.number').style.width = '30rem';
+
+        // when guess is too high
     } else if (guess > secretNumber) {
         if (score > 0) {
             document.querySelector('.message').textContent = '📈 Too high!';
@@ -27,6 +35,8 @@ document.querySelector('.check').addEventListener('click', function () {
             document.querySelector('.message').textContent = '💥 You lost the game!';
             document.querySelector('.score').textContent = 0;
         }
+
+        // when guess is too low
     } else if (guess < secretNumber) {
 
         if (score > 1) {
