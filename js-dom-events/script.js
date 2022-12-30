90998117a8152534f5aa73e496ed53879d19a31c
Aatif Sohel <84847205+aatifsohel@users.noreply.github.com>
2022-12-30 20:35:17 +0530

    implement high score

--- js-dom-events/script.js
+++ js-dom-events/script.js
@@ -1,17 +1,18 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.number').textContent = '?';
-document.querySelector('.score').textContent = 20;
+// document.querySelector('.number').textContent = '?';
+// document.querySelector('.score').textContent = 20;
 
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
-    console.log(typeof guess);
+    // console.log(typeof guess);
 
     // when there is no input
     if (!guess) {
@@ -24,6 +25,11 @@ document.querySelector('.check').addEventListener('click', function () {
 
         document.querySelector('body').style.backgroundColor = '#60b347';
         document.querySelector('.number').style.width = '30rem';
+
+        if (score > highScore) {
+            highScore = score;
+            document.querySelector('.highscore').textContent = highScore;
+        }
 
         // when guess is too high
     } else if (guess > secretNumber) {
