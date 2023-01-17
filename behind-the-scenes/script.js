beb608046de2c81b3bd131e6a8763870eb726a0a
Aatif Sohel <84847205+aatifsohel@users.noreply.github.com>
2023-01-17 16:43:01 +0530

    hoisting and tdz

--- behind-the-scenes/script.js
+++ behind-the-scenes/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 function calcAge(birthYear) {
     const age = 2037 - birthYear;
 
@@ -38,4 +39,29 @@ function calcAge(birthYear) {
 
 const firstName = 'master'
 calcAge(1991);
+*/
 
+// variables
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'master';
+let job = 'teacher';
+const year = 1991;
+
+// functions
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+console.log(addArrow);
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+    return a + b;
+}
+
+const addExpr = function (a, b) {
+    return a + b;
+}
+
+var addArrow = (a, b) => a + b;
