a10f2707e15585f91ce35cbb28e0aad866e8cb31
Aatif Sohel <84847205+aatifsohel@users.noreply.github.com>
2023-01-22 18:37:43 +0530

    primitives and reference types

--- behind-the-scenes/script.js
+++ behind-the-scenes/script.js
@@ -40,7 +40,7 @@ function calcAge(birthYear) {
 const firstName = 'master'
 calcAge(1991);
 */
-
+/*
 // variables
 console.log(me);
 // console.log(job);
@@ -65,3 +65,53 @@ const addExpr = function (a, b) {
 }
 
 var addArrow = (a, b) => a + b;
+*/
+/*
+console.log(this);
+
+const calcAge = function (birthYear) {
+    console.log(2037 - birthYear);
+    console.log(this);
+}
+
+calcAge(1991)
+
+const calcAgeArrow = birthYear => {
+    console.log(2037 - birthYear);
+    console.log(this);
+}
+
+calcAgeArrow(1988);
+*/
+/*
+const master = {
+    firstName: 'master',
+    year: 1991,
+    calcAge: function () {
+        console.log(this);
+        console.log(2037 - this.year);
+    },
+
+    greet: () => console.log(`hey ${this.firstName}`),
+}
+
+master.greet();
+console.log(this.firstName);
+*/
+
+let age = 30;
+let oldAge = age;
+age = 31;
+console.log(age);
+console.log(oldAge);
+
+const me = {
+    name: 'master',
+    age: 30,
+};
+
+const friend = me;
+friend.age = 32;
+
+console.log('Friend', friend);
+console.log('Me', me);
