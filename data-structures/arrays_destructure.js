41721b2533509bf7cf3ed7939f468ebc05238999
Aatif Sohel <84847205+aatifsohel@users.noreply.github.com>
2023-01-26 20:12:15 +0530

    revised: destructure arrays

---
+++ data-structures/arrays_destructure.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const restaurant = {
+    name: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23, Firenze, Italy',
+    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+    order: function (starterIndex, mainIndex) {
+        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+    },
+};
+
+// normal way of destructuring arrays using index
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+// destructuring arrays
+const [x, y, z] = arr;
+console.log(x, y, z);
+
+
+// Switching main into secondary OR swaping variables
+let [main, secondary] = restaurant.categories;
+console.log(main, secondary);
+
+// const temp = main; // old way
+// main = secondary;
+// secondary = temp;
+// console.log(main, secondary);
+
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+
+// Receive 2 return values from a function
+console.log(restaurant.order(2, 0));
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+const nested = [2, 4, [5, 6]];
+// const [i, , j] = nested;
+// console.log(i, j);
+
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+// Default values
+const [p, q, r] = [8, 9];
+console.log(p, q, r);
