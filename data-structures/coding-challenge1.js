d745b6c435a4b4e0db1a75c4b7651f466b14347f
Aatif Sohel <84847205+aatifsohel@users.noreply.github.com>
2023-01-29 17:41:14 +0530

    challenge 1 complete

--- data-structures/coding-challenge1.js
+++ data-structures/coding-challenge1.js
@@ -40,3 +40,41 @@ const game = {
         team2: 6.5,
     },
 };
+
+// #1
+const [players1, players2] = game.players;
+console.log(players1, players2);
+
+// #2
+const [team1gk, ...team1fieldplayers] = [...players1];
+const [team2gk, ...team2fieldplayers] = [...players2];
+
+console.log(team1gk, team1fieldplayers);
+console.log(team2gk, team2fieldplayers);
+
+// #3
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+
+// #4
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Periscic'];
+console.log(players1Final);
+
+// #5
+// const team1 = game.odds.team1;
+// const team2 = game.odds.team2;
+// const draw = game.odds.x;
+const { odds: { team1, x: draw, team2 } } = game;
+console.log(team1, team2, draw);
+
+// #6
+const printGoals = function (...players) {
+    console.log(`${players.length} goals were scored`);
+}
+
+printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+printGoals(...game.scored);
+
+// #7
+team1 < team2 && console.log(`Team 1 is more likely to win`);
+team1 > team2 && console.log(`Team 2 is more likely to win`);
