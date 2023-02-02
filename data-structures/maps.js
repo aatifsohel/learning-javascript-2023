3de9efa8ccddff761f388cb745c9ad57ca9f99b3
Aatif Sohel <84847205+aatifsohel@users.noreply.github.com>
2023-02-02 20:59:29 +0530

    maps - part 2

--- data-structures/maps.js
+++ data-structures/maps.js
@@ -39,3 +39,19 @@ const restaurant = {
         console.log(otherIngredients);
     },
 };
+
+// create map
+const rest = new Map();
+
+// add key/value pair
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+
+console.log(rest.set(2, 'Lisbon, Portugal'));
+
+rest.set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic']).set('open', 11).set('close', 23).set(true, 'We are open :D').set(false, 'We are closed :(');
+console.log(rest);
+
+const time = 21;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
