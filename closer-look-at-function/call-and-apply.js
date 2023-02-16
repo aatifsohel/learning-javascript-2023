cd4144a72ac29a50a6a9a6a1bd579c26a6f597d4
Aatif Sohel <84847205+aatifsohel@users.noreply.github.com>
2023-02-16 16:50:47 +0530

    bind method

--- closer-look-at-function/call-and-apply.js
+++ closer-look-at-function/call-and-apply.js
@@ -9,19 +9,19 @@ const lufthansa = {
         console.log(`${name} booked a seat on ${this.airline} flight ${this.iatacode}${flightNum}`);
 
 
-        this.bookings.push({flight: `${this.iatacode}${flightNum}`, name});
+        this.bookings.push({ flight: `${this.iatacode}${flightNum}`, name });
     },
-}
+};
 
-lufthansa.book(230, 'John Doe')
-lufthansa.book(563, 'Mevin D')
+lufthansa.book(230, 'John Doe');
+lufthansa.book(563, 'Mevin D');
 console.log(lufthansa.bookings);
 
 const eurowings = {
     airline: 'Eurowings',
     iatacode: 'EW',
     bookings: [],
-}
+};
 
 // copying method into a variable
 const book = lufthansa.book;
@@ -44,3 +44,45 @@ console.log(eurowings);
 
 book.call(eurowings, ...flightData); // Use spread operator method
 console.log(eurowings);
+
+// Bind Method
+const bookEW = book.bind(eurowings);
+const bookLH = book.bind(lufthansa);
+bookEW(12, 'Steven J');
+
+// using flight num specifically
+const bookEW23 = book.bind(eurowings, 23);
+bookEW23('Joey M');
+
+// With Event Listeners
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+    console.log(this);
+
+    this.planes++;
+    console.log(this.planes);
+};
+
+// lufthansa.buyPlane();
+
+document.querySelector('.buy').addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// Partial application
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23);
+// addVAT = value => value + value * 0.23;
+
+console.log(addVAT(100));
+console.log(addVAT(26));
+
+const addTaxRate = function (rate) {
+    return function (value) {
+        return value + value * rate;
+    };
+}
+
+const addVAT2 = addTaxRate(0.23);
+console.log(addVAT2(100));
+console.log(addVAT2(26));
