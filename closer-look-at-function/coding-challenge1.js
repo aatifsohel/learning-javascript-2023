8f301727fdb6d55f0de0952e05f3f5f3a3b6bf83
Aatif Sohel <84847205+aatifsohel@users.noreply.github.com>
2023-02-20 20:54:37 +0530

    coding challenge 1 - end

--- closer-look-at-function/coding-challenge1.js
+++ closer-look-at-function/coding-challenge1.js
@@ -39,7 +39,36 @@ Hints: Use many of the tools you learned about in this and the last section �
 
 const poll = {
     question: "What is your favourite programming language?",
-    options: ["0: JavaScript", "1: Python", "2: Rust", "3:C++"],
+    options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
     // This generates [0, 0, 0, 0]. More in the next section!
     answers: new Array(4).fill(0),
-    };
+    // #1 
+    registerNewAnswer() {
+        // #1.1 Getting user input
+        let userInput = Number(prompt(`${this.question}\n${this.options.join('\n')}\n(Write option number)`));
+        console.log(userInput);
+
+        // #1.2 Saving user input using shortciruiting
+        typeof userInput === 'number' && userInput < this.answers.length && this.answers[userInput]++;
+        console.log(this.answers);
+
+        // #4
+        this.displayResults();
+        this.displayResults('string');
+    },
+
+    // #2
+    displayResults (type = 'array') {
+        if (type === 'array') {
+            console.log(this.answers);
+        } else if (type === 'string') {
+            console.log(`Poll results are ${this.answers.join(', ')}`);
+        }
+    }
+};
+
+// #1
+
+document.querySelector('.poll').addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+poll.displayResults.call({answers: [5, 2, 3]}, 'string');
