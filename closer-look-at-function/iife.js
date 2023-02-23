a5469826ff2419bb691ef4c22d22b1f060a62924
Aatif Sohel <84847205+aatifsohel@users.noreply.github.com>
2023-02-23 20:04:01 +0530

    iife - end

--- closer-look-at-function/iife.js
+++ closer-look-at-function/iife.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const runOnce = function () {
+console.log('This will never run again');
+};
+
+runOnce();
+
+// IIFE 
+(function () {
+    console.log('This will never run again');
+})();
+
+(() => console.log('This will never run again'))();
+
+// variable scoping
+{
+    const isPrivate = 23;
+    var notPrivate = 46; 
+}
+
+// console.log(isPrivate);
+console.log(notPrivate);
