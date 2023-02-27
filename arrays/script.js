3d3784cec50dfec345b801d1fe2c9d9444502694
Aatif Sohel <84847205+aatifsohel@users.noreply.github.com>
2023-02-27 21:17:50 +0530

    forEach with Maps and Set

--- arrays/script.js
+++ arrays/script.js
@@ -65,11 +65,11 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
 
 // const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
@@ -104,7 +104,7 @@ console.log([...arr, ...arr2]);
 // * Join Method
 console.log(letters.join(' - ')); 
 */
-
+/*
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 // for (const movement of movements) {
@@ -128,3 +128,23 @@ movements.forEach(function (movement, index, arr) {
 // 0: function (200)
 // 1: function (450)
 // ...
+*/
+
+// forEach with Maps and Sets
+// Map
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+currencies.forEach(function (value, key, map) {
+  console.log(`${key}: ${value}`);
+});
+
+// Set
+const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+console.log(currenciesUnique);
+currenciesUnique.forEach(function(value, _, map){
+  console.log(`${value}: ${value}`);
+});
