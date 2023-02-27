1bb3828b5cc5da94b0f15742b7e4bb664b51c186
Aatif Sohel <84847205+aatifsohel@users.noreply.github.com>
2023-02-27 21:10:33 +0530

    for each

--- arrays/script.js
+++ arrays/script.js
@@ -71,10 +71,10 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
-
+/*
 let arr = ['a', 'b', 'c', 'd', 'e'];
 
 // * Slice Method - returns new array
@@ -102,4 +102,29 @@ console.log(letters);
 console.log([...arr, ...arr2]);
 
 // * Join Method
-console.log(letters.join(' - '));
+console.log(letters.join(' - ')); 
+*/
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// for (const movement of movements) {
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0 ){
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdraw ${Math.abs(movement)}`);
+  }
+}
+
+console.log('----FOR EACH----');
+movements.forEach(function (movement, index, arr) {
+  if (movement > 0 ){
+    console.log(`Movement ${index + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${index + 1}: You withdraw ${Math.abs(movement)}`);
+  }
+});
+
+// 0: function (200)
+// 1: function (450)
+// ...
