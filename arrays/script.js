abcbfef7c58bc2710399b2acad1ac876c03e37cb
Aatif Sohel <84847205+aatifsohel@users.noreply.github.com>
2023-03-02 22:38:07 +0530

    the magic of chaining method

--- arrays/script.js
+++ arrays/script.js
@@ -70,7 +70,7 @@ const displayMovements = function (movements) {
     const html = `
     <div class="movements__row">
           <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
-          <div class="movements__value">${mov}</div>
+          <div class="movements__value">${mov}€</div>
     </div>
     `;
 
@@ -82,8 +82,25 @@ displayMovements(account1.movements);
 
 const calcDisplayBalance = function (movements) {
   const balance = movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${balance} EUR`;
+  labelBalance.textContent = `${balance}€`;
 };
+
+const calcDisplaySummary = function (movements) {
+  const incomes = movements.filter(mov => mov > 0).reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes}€`;
+
+  const out = movements.filter(mov => mov < 0).reduce((acc, mov) => acc + mov, 0)
+  labelSumOut.textContent = `${Math.abs(out)}€`;
+  
+  const interest = movements.filter(mov => mov > 0).map(deposit => deposit * 1.2 / 100).filter((int, i, arr) => {
+    console.log(arr);
+    return int >= 1;
+  }).reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = `${interest}€`;
+
+};
+
+calcDisplaySummary(account1.movements);
 
 const createUsernames = function (accs) {
   accs.forEach(function (acc) {
@@ -249,21 +266,32 @@ console.log(movements);
     else return mov;
   }, movements[0]);
   console.log(max);
-  */
-
-// Coding Challenge 2
-
-const dogsJulia = [5, 2, 4, 1, 15, 8, 3];
-const dogsKate = [16, 6, 10, 5, 6, 1, 4];
-
-const calAverageHumanAge = function (arr) {
-  const dogsToHumanAge = arr.map(dogAge => dogAge <= 2 ? 2 * dogAge : 16 + dogAge * 4);
+  
+  // Coding Challenge 2
+  
+  const dogsJulia = [5, 2, 4, 1, 15, 8, 3];
+  const dogsKate = [16, 6, 10, 5, 6, 1, 4];
+  
+  const calAverageHumanAge = function (arr) {
+    const dogsToHumanAge = arr.map(dogAge => dogAge <= 2 ? 2 * dogAge : 16 + dogAge * 4);
   console.log(dogsToHumanAge);
   const dogsToHumanAdult = dogsToHumanAge.filter(humanAge => humanAge >= 18);
   console.log(dogsToHumanAdult);
   const avgHumanAge = dogsToHumanAdult.reduce((acc, age) => acc + age, 0);
-    return avgHumanAge / dogsToHumanAdult.length;
+  return avgHumanAge / dogsToHumanAdult.length;
 };
 
 console.log(calAverageHumanAge(dogsJulia));
 console.log(calAverageHumanAge(dogsKate));
+*/
+
+// Pipeline
+const eurToUsd = 1.1;
+// const totalDepositsUSD = movements.filter(mov => mov > 0).map(mov => mov * eurToUsd).reduce((acc, mov) => acc + mov, 0);
+
+// to display individual array while using pipeline
+const totalDepositsUSD = movements.filter(mov => mov > 0).map((mov, i, arr) => {
+  // console.log(arr);
+  return mov * eurToUsd}).reduce((acc, mov) => acc + mov, 0);
+
+console.log(totalDepositsUSD);
