601cec0cd5aaee9581ad54a54e55909aeb6ad176
Aatif Sohel <84847205+aatifsohel@users.noreply.github.com>
2023-03-03 15:59:34 +0530

    find method

--- arrays/script.js
+++ arrays/script.js
@@ -294,14 +294,22 @@ const totalDepositsUSD = movements.filter(mov => mov > 0).map((mov, i, arr) => {
   return mov * eurToUsd}).reduce((acc, mov) => acc + mov, 0);
   
   console.log(totalDepositsUSD);
+  
+  // Coding Challenge 3
+  
+  const dogsJulia = [5, 2, 4, 1, 15, 8, 3];
+  const dogsKate = [16, 6, 10, 5, 6, 1, 4];
+  
+  const calcAvg = arr => arr.map(mov => mov <= 2 ? 2 * mov : 16 + mov * 4).filter(age => age >= 18).reduce((acc, adultAge, i, arr) => acc + adultAge / arr.length, 0);
+  
+  console.log(`pipeline`);
+  console.log(calcAvg(dogsJulia), calcAvg(dogsKate));
   */
 
-// Coding Challenge 3
+  // Find Method
+  const firstWidthrawal = movements.find(mov => mov < 0);
+  console.log(firstWidthrawal);
 
-const dogsJulia = [5, 2, 4, 1, 15, 8, 3];
-const dogsKate = [16, 6, 10, 5, 6, 1, 4];
-
-const calcAvg = arr => arr.map(mov => mov <= 2 ? 2 * mov : 16 + mov * 4).filter(age => age >= 18).reduce((acc, adultAge, i, arr) => acc + adultAge / arr.length, 0);
-
-console.log(`pipeline`);
-console.log(calcAvg(dogsJulia), calcAvg(dogsKate));
+  console.log(accounts);
+  const account = accounts.find(acc => acc.owner === 'Jessica Davis');
+  console.log(account);
