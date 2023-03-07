5e37332921e0d23de5e39dfc7360b5641896a824
Aatif Sohel <84847205+aatifsohel@users.noreply.github.com>
2023-03-07 14:57:41 +0530

    coding challenge 4 - end

--- arrays/script.js
+++ arrays/script.js
@@ -489,4 +489,40 @@ const dogs = [
   { weight: 8, curFood: 200, owners: ['Matilda'] },
   { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
   { weight: 32, curFood: 340, owners: ['Michael'] },
-  ];
+];
+
+// #1 Create new property for dogs
+dogs.forEach(dog => dog.recommendedFood = Math.trunc(dog.weight ** 0.75 * 28));
+console.log(dogs);
+
+// #2 Sarah's Dog
+const dogSarah = dogs.find(dog => dog.owners.includes('Sarah'));
+console.log(dogSarah);
+console.log(`Sarah's dog eating too ${dogSarah.curFood > dogSarah.recommendedFood ? 'much' : 'little'}`);
+
+// #3
+const ownersEatingTooMuch = dogs.filter(dog => 
+  dog.curFood > dog.recommendedFood).flatMap(dog => dog.owners);
+console.log(ownersEatingTooMuch);
+
+const ownersEatingTooLittle = dogs.filter(dog => 
+  dog.curFood < dog.recommendedFood).flatMap(dog => dog.owners);
+console.log(ownersEatingTooLittle);
+
+// #4
+console.log(`${ownersEatingTooMuch.join(' and ')}'s dogs eat too much.`);
+console.log(`${ownersEatingTooLittle.join(' and ')}'s dogs eat too little.`);
+
+// #5
+console.log(dogs.some(dog => dog.curFood === dog.recommendedFood));
+
+// #6
+const checkEatingOK = dog => dog.curFood > dog.recommendedFood * 0.9 && dog.curFood < dog.recommendedFood * 1.1
+console.log(dogs.some(checkEatingOK));
+
+// #7
+console.log(dogs.filter(checkEatingOK));
+
+// #8
+const dogsSorted = dogs.slice().sort((a, b) => a.recommendedFood - b.recommendedFood);
+console.log(dogsSorted);
