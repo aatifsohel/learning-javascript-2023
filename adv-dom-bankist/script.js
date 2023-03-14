e606499533ece497378173bebf6af52c54a6e1fd
Aatif Sohel <84847205+aatifsohel@users.noreply.github.com>
2023-03-14 20:50:04 +0530

    select, create, delete html elements

--- adv-dom-bankist/script.js
+++ adv-dom-bankist/script.js
@@ -29,3 +29,48 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+/*
+ **************************************************
+ */
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section'); // returns nodelist
+console.log(allSections);
+
+document.getElementById('section--1');
+
+// returns HTML Collection & it changes if you remove from html document
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn'));
+
+// creating & inserting elements
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+// message.textContent = 'We use cookies for improved functionality and analytics.';
+message.innerHTML =
+  'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+
+// only one could be shown in web page ↓
+header.prepend(message); // adds first child of selected element
+// header.append(message); // adds last child of selected element
+
+// duplicating the same element at multiple places in web page ↓
+// header.append(message.cloneNode(true));
+
+// before & after ↓
+// header.before(message);
+// header.after(message);
+
+// Deleting elements
+document.querySelector('.btn--close-cookie').addEventListener('click', function() {
+  message.remove();
+
+  // old way of removing element
+  // message.parentElement.removeChild(message);
+})
