c770e63206274060933f2e0f3bd7bde20e22f110
Aatif Sohel <84847205+aatifsohel@users.noreply.github.com>
2023-04-12 17:07:06 +0530

    constructor function and new operator - 2

--- oop-in-js/script.js
+++ oop-in-js/script.js
@@ -1,11 +1,27 @@
 'use strict';
 
 const Person = function (firstName, birthYear) {
-    this.firstName  = firstName;
-    this.birthYear = birthYear;
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // never do this
+  //   this.calcAge = function () {
+  //     console.log(2037 - this.birthYear);
+  //   };
 };
 
 const john = new Person('John', 1999);
-
 console.log(john);
+
+// 1. New {object} created
+// 2. function is called, this => {object}
+// 3. {object} linked to prototype
+// 4. function automatically return {object}
+
+const jane = new Person('Jane', 2013);
+const matt = new Person('Matt', 2003);
+console.log(jane, matt);
+
 console.log(john instanceof Person);
+
+// Prototypes
