70e0ab42f5597dbf818fb56990cf74ccf4f948b8
Aatif Sohel <84847205+aatifsohel@users.noreply.github.com>
2023-04-12 15:53:58 +0530

    constructor function and new operator

--- oop-in-js/script.js
+++ oop-in-js/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const Person = function (firstName, birthYear) {
+    this.firstName  = firstName;
+    this.birthYear = birthYear;
+};
+
+const john = new Person('John', 1999);
+
+console.log(john);
+console.log(john instanceof Person);
