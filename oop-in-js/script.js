8ec2a3ddfca8898c22296291e600d23b9a5c7b91
Aatif Sohel <84847205+aatifsohel@users.noreply.github.com>
2023-04-19 22:46:25 +0530

    coding challenge 3

--- oop-in-js/script.js
+++ oop-in-js/script.js
@@ -275,3 +275,45 @@ console.log(mike instanceof Object);
 // Important: pointing back constructor to Student from Person
 Student.prototype.constructor = Student;
 console.dir(Student.prototype.constructor);
+
+//* Coding Challenge
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(this.speed);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(this.speed);
+};
+
+const EV = function (make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
+};
+
+// link the protypes
+EV.prototype = Object.create(Car.prototype);
+
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+
+EV.prototype.accelerate = function () {
+  this.speed += 20;
+  this.charge--;
+  console.log(`Speed: ${this.speed}, Charge: ${this.charge};`);
+};
+
+const tesla = new EV('Tesla', 120, 23);
+tesla.chargeBattery(90);
+console.log(tesla);
+
+tesla.brake();
+tesla.accelerate();
