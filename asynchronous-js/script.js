e2cc47899401d1cd6846cc9c46797f0212595a52
Aatif Sohel <84847205+aatifsohel@users.noreply.github.com>
2023-05-08 20:11:49 +0530

    callback hell

--- asynchronous-js/script.js
+++ asynchronous-js/script.js
@@ -6,57 +6,31 @@ const countriesContainer = document.querySelector('.countries');
 ///////////////////////////////////////
 // old way of doing AJAX
 
-const getCountryData = function (country) {
-  const request = new XMLHttpRequest();
-  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
-  request.send();
+const renderCountry = function (data, className = '') {
+  // Destructuring
+  const {
+    name: { common },
+    currencies,
+    flags,
+    population,
+    languages,
+    region,
+  } = data;
 
-  let data;
-  request.addEventListener('load', function () {
-    //   console.log(this.responseText);
+  //   console.log(common, population, region);
+  //   console.log(Object.values(currencies)[0].name);
+  //   console.log(Object.values(flags)[0]);
+  //   console.log(Object.values(languages)[0]);
 
-    [data] = JSON.parse(this.responseText);
-    // console.log(data);
-
-    // Destructuring
-    const {
-      name: { common },
-      currencies,
-      flags,
-      population,
-      languages,
-      region,
-    } = data;
-
-    //   console.log(common, population, region);
-    //   console.log(Object.values(currencies)[0].name);
-    //   console.log(Object.values(flags)[0]);
-    //   console.log(Object.values(languages)[0]);
-
-    //   const html = `
-    //     <article class="country">
-    //           <img class="country__img" src="${data.flags.png}" />
-    //           <div class="country__data">
-    //             <h3 class="country__name">${data['name']['common']}</h3>
-    //             <h4 class="country__region">${data.region}</h4>
-    //             <p class="country__row"><span>👫</span>${(
-    //               +data.population / 1000000
-    //             ).toFixed(1)}</p>
-    //             <p class="country__row"><span>🗣️</span>${data.languages.por}</p>
-    //             <p class="country__row"><span>💰</span>${data.currencies[0].name}
-    //             </p>
-    //           </div>
-    //         </article>
-    //     `;
-    const html = `
-    <article class="country">
+  const html = `
+    <article class="country ${className}">
           <img class="country__img" src="${Object.values(flags)[0]}" />
           <div class="country__data">
             <h3 class="country__name">${common}</h3>
             <h4 class="country__region">${region}</h4>
             <p class="country__row"><span>👫</span>${(
               +population / 1000000
-            ).toFixed(1)}</p>
+            ).toFixed(1)} people</p>
             <p class="country__row"><span>🗣️</span>${
               Object.values(languages)[0]
             }</p>
@@ -68,11 +42,58 @@ const getCountryData = function (country) {
         </article>
     `;
 
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbour = function (country) {
+  // AJAX call country 1
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    //   console.log(this.responseText);
+
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    // Render country 1
+    renderCountry(data);
+
+    // Get neighbour country 2
+    const [neighbour] = data.borders;
+    console.log(neighbour);
+
+    if (!neighbour) return;
+
+    // AJAX call country 2
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      const [data2] = JSON.parse(this.responseText);
+      console.log(data2);
+
+      renderCountry(data2, 'neighbour');
+    });
   });
 };
 
-getCountryData('portugal')
-getCountryData('usa')
-getCountryData('germany')
+// getCountryAndNeighbour('portugal');
+getCountryAndNeighbour('usa');
+
+// Callback hell -> nested cb (bad code)
+setTimeout(() => {
+  console.log('1 second passed');
+  setTimeout(() => {
+    console.log('2 second passed');
+    setTimeout(() => {
+      console.log('3 second passed');
+      setTimeout(() => {
+        console.log('4 second passed');
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
