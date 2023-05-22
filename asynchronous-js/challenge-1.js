f6a87c7a995fa28ac169bbec7729d579bd9baa52
Aatif Sohel <84847205+aatifsohel@users.noreply.github.com>
2023-05-22 14:58:50 +0530

    coding challenge 2 - end

--- asynchronous-js/challenge-1.js
+++ asynchronous-js/challenge-1.js
@@ -4,7 +4,7 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
-
+/*
 const renderCountry = function (data, className = '') {
   // Destructuring
   const {
@@ -75,3 +75,49 @@ const whereAmI = function (lat, lng) {
 whereAmI(19.037, 72.873);
 whereAmI(52.508, 13.381);
 whereAmI(-33.933, 18.474);
+*/
+
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+const imgContainer = document.querySelector('.images');
+
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement('img');
+    img.src = imgPath;
+
+    img.addEventListener('load', function () {
+      imgContainer.append(img);
+      resolve(img);
+    });
+
+    img.addEventListener('error', function () {
+      reject(new Error('Image not found'));
+    });
+  });
+};
+
+let currentImg;
+createImage('img/img-1.jpg')
+  .then(img => {
+    currentImg = img;
+    console.log('Image 1 loaded');
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+    return createImage('img/img-2.jpg');
+  })
+  .then(img => {
+    currentImg = img;
+    console.log('Image 2 loaded');
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+  })
+  .catch(err => console.error(err));
