a8345d92acf725fd4a36c1c261f93167e5b968cb
Aatif Sohel <84847205+aatifsohel@users.noreply.github.com>
2023-05-23 23:12:24 +0530

    running promises in parallel

--- asynchronous-js/script.js
+++ asynchronous-js/script.js
@@ -324,6 +324,7 @@ const whereAmI = function () {
 
 btn.addEventListener('click', whereAmI);
 */
+/*
 const getPosition = function () {
   return new Promise(function (resolve, reject) {
     navigator.geolocation.getCurrentPosition(resolve, reject);
@@ -387,3 +388,34 @@ console.log('1: WILL GET LOCATION');
   }
   console.log('3: FINISHED GETTING LOCATION');
 })();
+*/
+
+const getJSON = function (url, errorMsg = 'Something went wrong') {
+  return fetch(url).then(response => {
+    if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+
+    return response.json();
+  });
+};
+
+const get3Countries = async function (c1, c2, c3) {
+  try {
+    // these are running one after another
+    // const [data1] = await getJSON(`https://restcountries.com/v3.1/name/${c1}`);
+    // const [data2] = await getJSON(`https://restcountries.com/v3.1/name/${c2}`);
+    // const [data3] = await getJSON(`https://restcountries.com/v3.1/name/${c3}`);
+    // console.log([data1.capital, data2.capital, data3.capital]);
+
+    // running in parallel (same time)
+    const data = await Promise.all([
+      getJSON(`https://restcountries.com/v3.1/name/${c1}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c2}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c3}`),
+    ]);
+    console.log(data.map(d => d[0].capital));
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+get3Countries('portugal', 'canada', 'tanzania');
